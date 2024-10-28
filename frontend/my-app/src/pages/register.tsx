66dafb725ba885b328d6f75ba55418750ef3fc7d
ramlow07 <luamramlow@gmail.com>
2024-10-28 20:39:54 -0300

    feature: adding register page and starting home page

---
+++ frontend/my-app/src/pages/register.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function RegisterPage() {
+  return (
+    <div className="main-container flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="login-container bg-white p-8 rounded shadow-md w-80">
+        <input
+          type="text"
+          placeholder="Email"
+          className="mb-4 w-full p-2 border border-gray-300 rounded"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="mb-4 w-full p-2 border border-gray-300 rounded"
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className="mb-4 w-full p-2 border border-gray-300 rounded"
+        />
+        <button className="w-full bg-blue-500 text-white py-2 rounded">
+          Register
+        </button>
+      </div>
+      <div className="register mt-4">
+        <p>
+          Already have an account?{" "}
+          <button className="text-blue-500 underline">
+            Click here to login
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterPage;
