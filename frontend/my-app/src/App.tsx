5fbaad468ec1e19f181a3f0f9e542712732599c1
ramlow07 <luamramlow@gmail.com>
2024-10-28 21:06:23 -0300

    feature: adding homepage

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -3,27 +3,31 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
+import HomePage from "./pages/home";
 // TODO: @COMPONENTS: REGISTER AND HOME PAGE.
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/">Register</Link>
-          </li>
-          <li>
-            <Link to="/">Login</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="navbar flex flex-row justify-center align-center">
+        <nav className="flex space-y-4">
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
