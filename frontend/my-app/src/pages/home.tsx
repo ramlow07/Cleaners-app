d5cb6d9ae1bf21d4c30fd7568f767ec6446b6de8
ramlow07 <luamramlow@gmail.com>
2025-01-28 20:16:53 -0300

    adding footer and SocialProofSection to the homepage

--- frontend/my-app/src/pages/home.tsx
+++ frontend/my-app/src/pages/home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
+import SocialProofSection from "../components/SocialProofSection";
+import Footer from "../components/Footer";
 const HomePage = () => {
   return (
     <div className="homepage">
       <HeroSection />
+      <SocialProofSection />
+      <Footer />
     </div>
   );
 };
