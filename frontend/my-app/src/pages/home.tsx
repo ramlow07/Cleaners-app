d2a7f4e9d8d3e4aea3fd36bf4c2bf01a2b8bf1ab
ramlow07 <luamramlow@gmail.com>
2025-01-28 21:08:42 -0300

    feat: adding benefits section to the homepage

--- frontend/my-app/src/pages/home.tsx
+++ frontend/my-app/src/pages/home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import SocialProofSection from "../components/SocialProofSection";
 import Footer from "../components/Footer";
+import BenefitsSection from "../components/BenefitsSection";
 const HomePage = () => {
   return (
     <div className="homepage">
       <HeroSection />
       <SocialProofSection />
+      <BenefitsSection />
       <Footer />
     </div>
   );
