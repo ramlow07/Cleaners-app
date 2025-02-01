6f55248b70d7854a20632ddb0970f5ad3b9d1397
ramlow07 <luamramlow@gmail.com>
2025-01-31 21:33:52 -0300

    feat: adding post cleaner function to the api

--- backend/routes/cleaner.js
+++ backend/routes/cleaner.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-const Cleaner = require("..models/cleanr");
+const Cleaner = require("../models/cleaner");
+
+module.exports = router;
 
 router.get("/", async (req, res) => {
   try {
@@ -9,5 +11,27 @@ router.get("/", async (req, res) => {
     res.status(200).json(cleaners);
   } catch (err) {
     res.status(500).json({ message: "Error fetching cleaners", error: err });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const cleaner = await Cleaner.findById(req.params.id);
+    if (!cleaner) {
+      return res.status(404).json({ message: "Cleaner not found." });
+    }
+    res.status(200).json(cleaner);
+  } catch (err) {
+    res.status(500).json({ message: "Error finding cleaner", error: err });
+  }
+});
+
+router.post("/cleaner", async (req, res) => {
+  try {
+    const newCleaner = new Cleaner(req.body);
+    await newCleaner.save();
+    res.status(201).json(newCleaner);
+  } catch (err) {
+    res.status(400).json({ message: "Error creating cleaner", error: err });
   }
 });
