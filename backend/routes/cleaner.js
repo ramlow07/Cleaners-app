2d35676f1a6178332c35d183951e1873e4fe38a8
ramlow07 <luamramlow@gmail.com>
2025-02-04 21:32:15 -0300

    feat: adding delete cleaners function and finalizing CRUD

--- backend/routes/cleaner.js
+++ backend/routes/cleaner.js
@@ -58,4 +58,22 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const cleanerId = req.params.id;
+    const cleanerIdVerify = await Cleaner.findById(cleanerId);
+
+    if (!cleanerIdVerify) {
+      return res.status(404).json({ message: "Error finding cleaner." });
+    }
+
+    await Cleaner.findByIdAndDelete(cleanerId);
+    return res.status(200).json({ message: "Cleaner deleted successfully" });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Error deleting cleaner.", error: err });
+  }
+});
+
 module.exports = router;
