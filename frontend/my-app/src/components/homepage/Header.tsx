76bfc46d43db6f99139727e26aec1700314f28b8
ramlow07 <luamramlow@gmail.com>
2025-02-06 21:23:49 -0300

    feature: adding underline effect to the header buttons

--- frontend/my-app/src/components/homepage/Header.tsx
+++ frontend/my-app/src/components/homepage/Header.tsx
@@ -8,17 +8,26 @@ const Header = () => {
         <nav>
           <ul className="flex space-x-10 text-lg font-medium">
             <li>
-              <Link to="/home" className="hover:text-green-500 transition">
+              <Link
+                to="/home"
+                className="relative px-4 py-2 text-green-900 after:block after:h-[2px] after:w-0 after:bg-green-500 after:transition-all after:duration-300 hover:after:w-full"
+              >
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/register" className="hover:text-green-500 transition">
+              <Link
+                to="/register"
+                className="relative px-4 py-2 text-green-900 after:block after:h-[2px] after:w-0 after:bg-green-500 after:transition-all after:duration-300 hover:after:w-full"
+              >
                 Register
               </Link>
             </li>
             <li>
-              <Link to="/login" className="hover:text-green-500 transition">
+              <Link
+                to="/login"
+                className="relative px-4 py-2 text-green-900 after:block after:h-[2px] after:w-0 after:bg-green-500 after:transition-all after:duration-300 hover:after:w-full"
+              >
                 Login
               </Link>
             </li>
