09c202a307514ddf8aa5ad4cc8585db7585183a7
ramlow07 <luamramlow@gmail.com>
2025-02-12 20:41:41 -0300

    fix: updating api route for fetch cleaners

--- frontend/my-app/src/components/cleanerList/cleanerList.tsx
+++ frontend/my-app/src/components/cleanerList/cleanerList.tsx
@@ -4,7 +4,7 @@ export default function CleanerList() {
   const [cleaner, setCleaners] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/cleaners")
+    fetch("http://localhost:8080/cleaners")
       .then((res) => res.json())
       .then((data) => setCleaners(data))
       .catch((error) => console.error("Error fetching cleaners", error));
