85d5e6565c0268d67283dc4c9903a9a76fa8c12a
ramlow07 <luamramlow@gmail.com>
2025-02-12 20:37:24 -0300

    feat: adding useEffect to fetch cleaners when component mounts

--- frontend/my-app/src/components/cleanerList/cleanerList.tsx
+++ frontend/my-app/src/components/cleanerList/cleanerList.tsx
@@ -2,4 +2,11 @@ import { useState, useEffect } from "react";
 
 export default function CleanerList() {
   const [cleaner, setCleaners] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/cleaners")
+      .then((res) => res.json())
+      .then((data) => setCleaners(data))
+      .catch((error) => console.error("Error fetching cleaners", error));
+  }, []);
 }
