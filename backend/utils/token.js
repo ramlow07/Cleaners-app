2881b3b11297570df16f6c35356f05756cba6e91
ramlow07 <luamramlow@gmail.com>
2025-02-12 19:49:17 -0300

    fix: debugging a typing error on token.js

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,29 +1,18 @@
+require("dotenv").config();
 const { sign } = require("jsonwebtoken");
 // signing the access token
 
 const createAccessToken = (id) => {
-  return (
-    sign({ id }),
-    proccess.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: 15 * 60,
-    }
-  );
+  return sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: 15 * 60,
+  });
 };
-
-// signing the refresh token
 
 const createRefreshToken = (id) => {
-  return (
-    sign({ id }),
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: "90d",
-    }
-  );
+  return sign({ id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "90d",
+  });
 };
-
-// sending the access token to the client
 
 const sendAccessToken = (_req, res, accesstoken) => {
   res.json({
