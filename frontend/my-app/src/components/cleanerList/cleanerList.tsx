fd204e62a7eed6ac074814edef9769936ee10c34
ramlow07 <luamramlow@gmail.com>
2025-02-13 21:20:36 -0300

    refactor: using async instead of .then on fetchCleaners to optimize the code

--- frontend/my-app/src/components/cleanerList/cleanerList.tsx
+++ frontend/my-app/src/components/cleanerList/cleanerList.tsx
@@ -1,12 +1,45 @@
 import { useState, useEffect } from "react";
 
+type CleanerType = {
+  id: string;
+  name: string;
+};
+
 export default function CleanerList() {
-  const [cleaner, setCleaners] = useState([]);
+  const [cleaner, setCleaners] = useState<CleanerType[]>([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8080/cleaners")
-      .then((res) => res.json())
-      .then((data) => setCleaners(data))
-      .catch((error) => console.error("Error fetching cleaners", error));
-  }, []);
+    const fetchCleaners = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/cleaners");
+        const cleanersData: CleanerType[] = await res.json();
+        setCleaners(cleanersData);
+      } catch (error) {
+        console.error("Error fetching cleaners.", error);
+      }
+    };
+
+    fetchCleaners();
+  });
+
+  const filteredCleaners = cleaner.filter((cleaner) =>
+    cleaner.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search cleaners..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <ul>
+        {filteredCleaners.map((cleaner) => (
+          <li key={cleaner.id}>{cleaner.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
