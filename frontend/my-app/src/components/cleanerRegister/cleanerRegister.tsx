bc1937713f0f3abc1be09ae895d84ec143f57372
ramlow07 <luamramlow@gmail.com>
2025-02-16 20:44:03 -0300

    feat: adding more skill checkboxes

--- frontend/my-app/src/components/cleanerRegister/cleanerRegister.tsx
+++ frontend/my-app/src/components/cleanerRegister/cleanerRegister.tsx
@@ -6,7 +6,7 @@ function CleanerRegister() {
   const navigate = useNavigate();
 
   const cleanerServicesList = [
-    "Deep Cleaning",
+    "House Cleaning",
     "House Cleaning",
     "Apartment Cleaning",
   ];
@@ -23,12 +23,13 @@ function CleanerRegister() {
   });
   const [message, setMessage] = useState("");
 
-  const handleCheckboxChange = (services: string) => {
-    setSelectedServices((prev) =>
-      prev.includes(services)
-        ? prev.filter((s) => s !== services)
-        : [...prev, services]
-    );
+  const handleCheckboxChange = (skill: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      services: prev.services.includes(skill)
+        ? prev.services.filter((s) => s !== skill)
+        : [...prev.services, skill],
+    }));
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -90,18 +91,55 @@ function CleanerRegister() {
             required
             className="mb-4 w-full p-2 border border-gray-300 rounded"
           />
-          <label htmlFor="">
-            <input
-              type="checkbox"
-              name="Deep Cleaning"
-              checked={formData.services.includes("Deep Cleaning")}
-              value={formData.services}
-              onChange={() => handleCheckboxChange("DeepCleaning")}
-              required
-              className="mb-4 w-full p-2 border border-gray-300 rounded"
-            />
-            Deep Cleaning
-          </label>
+          <div className="label flex items-center gap-2">
+            <label
+              htmlFor=""
+              className="flex flex-row-reverse gap-2 items-center cursor-pointer my-6 border border-spacing-2"
+            >
+              <input
+                type="checkbox"
+                name="House Cleaning"
+                checked={formData.services.includes("House Cleaning")}
+                onChange={() => handleCheckboxChange("House Cleaning")}
+                required
+                className="mb-4 w-full p-2 border border-gray-300 rounded"
+              />
+              Deep Cleaning
+            </label>
+          </div>
+
+          <div className="label flex items-center gap-2">
+            <label
+              htmlFor=""
+              className="flex flex-row-reverse gap-2 items-center cursor-pointer my-2 border border-spacing-2"
+            >
+              <input
+                type="checkbox"
+                name="House Cleaning"
+                checked={formData.services.includes("House Cleaning")}
+                onChange={() => handleCheckboxChange("House Cleaning")}
+                required
+                className="mb-4 w-full p-2 border border-gray-300 rounded"
+              />
+              House Cleaning
+            </label>
+          </div>
+          <div className="label flex items-center gap-2">
+            <label
+              htmlFor=""
+              className="flex flex-row-reverse gap-2 items-center cursor-pointer my-2 border border-spacing-2"
+            >
+              <input
+                type="checkbox"
+                name="Apartment Cleaning"
+                checked={formData.services.includes("Apartment Cleaning")}
+                onChange={() => handleCheckboxChange("Apartment Cleaning")}
+                required
+                className="mb-4 w-full p-2 border border-gray-300 rounded"
+              />
+              Apartment Cleaning
+            </label>
+          </div>
           <button
             type="submit"
             className="mb-4 w-full p-2 border border-blue-300 rounded"
