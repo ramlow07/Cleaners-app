6d87356f88a6443ac48c294034982ea8ea444cd4
ramlow07 <luamramlow@gmail.com>
2025-04-07 20:53:53 -0300

    feat: adding buttons to choose-role page

--- apps/cleaners-web/src/app/auth/choose-role/page.tsx
+++ apps/cleaners-web/src/app/auth/choose-role/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 // import { UserAuthFormLogin } from "./components/user-auth-form"
 
 export default function ChooseRole() {
@@ -61,29 +62,17 @@ export default function ChooseRole() {
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to login.
+              <h1 className="text-2xl font-bold tracking-tight">
+                Welcome to CleanersAPP!
+              </h1>
+              <p className="text-md font-semibold text-muted-foreground">
+                Are you a CLEANER or a CONTRACTOR?
               </p>
+              <div className="">
+                <Button>I'm a CLEANER.</Button>
+                <Button>I'm a CONTRACTOR.</Button>
+              </div>
             </div>
-
-            <p className="px-8 text-sm text-center text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
           </div>
         </div>
       </div>
